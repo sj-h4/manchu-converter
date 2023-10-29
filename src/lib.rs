e2b0b0c26ce2bba116c11acd3d4a622ebbb670b4
sj <64534668+sj-h4@users.noreply.github.com>
2023-10-29 18:41:07 +0900

    Fix settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-mod convert;
+pub mod convert;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
@@ -10,7 +7,5 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
